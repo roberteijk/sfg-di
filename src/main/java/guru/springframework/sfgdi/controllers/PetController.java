f814aa989cf7b3e98828b571e2ba19dd29346e32
Robert van den Eijk <robert@vandeneijk.net>
2020-02-29 11:11:54 +0100

    Removed unnecessary @Qualifier annotation from answer to Assignment 4.

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,7 +1,6 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.PetService;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -13,7 +12,7 @@ public class PetController {
 
     private final PetService petService;
 
-    public PetController(@Qualifier("petService") PetService petService) {
+    public PetController(PetService petService) {
         this.petService = petService;
     }
 
